ae620e0d11c9b631302275ff8c9ba39433bc06d5
Kuulhaiku <47490419+Kuulhaiku@users.noreply.github.com>
2019-02-14 16:57:34 -0600

    Update threadtest.cc
    Copy the included libraries to the top of your version, 
    copy the globals section into the appropriate spot, 
    copy the functions into the proper location. 
    Update your system.cc to include input for this task( -A 6)
    Create a conditional branch in threadTest.cc root function for when input is 6

--- threadtest.cc
+++ threadtest.cc
@@ -1,21 +1,8 @@
-// threadtest.cc
-//	Simple test case for the threads assignment.
-//
-//	Create two threads, and have them context switch
-//	back and forth between themselves by calling Thread::Yield,
-//	to illustratethe inner workings of the thread system.
-//
-// Copyright (c) 1992-1993 The Regents of the University of California.
-// All rights reserved.  See copyright.h for copyright notice and limitation
-// of liability and disclaimer of warranty provisions.
-
-#include "copyright.h"
-#include "system.h"
-
-//---------------------Begin changes by Chau Cao--------------------
 #include <string.h>
 #include <ctype.h>
 #include <synch.h>
+
+//---------------------Begin changes by Chau Cao--------------------
 //Global Variables to be used with Project 2 task 4: PO Sim w/ Semaphores
 //Struct declaration to define mail and post office
 //Semaphores declared to protect critical resources
@@ -30,8 +17,10 @@ struct postOffice {
   int totalMessages;
   int messagesSent;
   int messagesRead;
+  bool * deadlockMonitor;
+  int deadlockCount;
+  Semaphore * deadlock;
   Semaphore * writer;
-  //Semaphore * reader;
   int * mailCount;
   mail ** mailbox;
   const char * messageArray[13] = {
@@ -55,272 +44,6 @@ Semaphore ** freeSpaceSemaphore;
 postOffice poss;
 //------------------------End Changes by Chau Cao------------------------
 
-//----------------------------------------------------------------------
-// SimpleThread
-// 	Loop 5 times, yielding the CPU to another ready thread
-//	each iteration.
-//
-//	"which" is simply a number identifying the thread, for debugging
-//	purposes.
-//----------------------------------------------------------------------
-
-void
-SimpleThread(int which)
-{
-    int num;
-
-    for (num = 0; num < 5; num++) {
-	      printf("*** thread %d looped %d times\n", which, num);
-
-          currentThread->Yield();
-    }
-
-}
-
-/*
-InputTest
-Is a void function that takes input from the user and determines the type of input.
-For example an input of "5" would be an integer. While an input of "abc" is an array of characters.
-The max input size is 1024 characters.
-To check this a flag is set if certain parameters are met.
-Is there is a number it is flagged as an integer.
-If there is a negative sign at the from it is flagged negative.
-If there is only one decimal point it is flagged as a decimal. If there is two it is flagged as a string.
-All other cases default to a character string.
-*/
-void
-InputTest(int a)
-{
-    printf("Enter an input with a max size of 1024: ");
-
-    char userInput[1025];
-    fgets(userInput, 1025, stdin);
-
-    printf("Input is: %s\n", userInput);
-
-    int count = 0;
-
-    // Flags if input can be negative
-    bool isNegative = FALSE;
-    // Flags if input can be an integer
-    bool isInt = FALSE;
-    // Flags if input can be a decimal
-    bool isDec = FALSE;
-    // checks if a period char has bee used already
-    bool decPeriod = FALSE;
-    // Flags if input is a String
-    bool isString = FALSE;
-
-    if(userInput[0] == '-')
-    {
-      isNegative = TRUE;
-      count++;
-    }
-
-    while('\0' != userInput[count])
-    {
-      //Checks if current character is a digit
-      if(userInput[count] == '0' || userInput[count] == '1' || userInput[count] == '2' || userInput[count] == '3' || userInput[count] == '4' || userInput[count] == '5'
-      || userInput[count] == '6' || userInput[count] == '7' || userInput[count] == '8' || userInput[count] == '9')
-      {
-        isInt = TRUE;
-      }
-      // Checks if there has been a digit used adn if there has already been a period
-      else if(userInput[count] == '.' && isInt && decPeriod != TRUE)
-      {
-
-        decPeriod = TRUE;
-        isDec = TRUE;
-
-        // Handles the edge case of a sequence of number folllowed by a period
-        if(userInput[count+1] == '\n')
-        {
-          isString = TRUE;
-        }
-
-      //Removes the new line from being counted and marking all other symbols as a string
-      }else if(userInput[count] != '\n')
-      {
-        isString = TRUE;
-      }
-
-      count++;
-    }
-
-    //Prints to console what type of input the user typed depending on flags set
-    if(isInt && isNegative != TRUE && isDec != TRUE && isString != TRUE)
-    {
-      printf("This is a integer\n");
-    }else if(isInt && isNegative && isDec != TRUE && isString != TRUE)
-    {
-      printf("This is a negative integer\n");
-    }else if(isInt && isNegative != TRUE && isDec && isString != TRUE)
-    {
-      printf("This is a decimal\n");
-    }else if(isInt && isNegative && isDec && isString != TRUE)
-    {
-      printf("This is a negative decimal\n");
-    }else
-    {
-      printf("This is a character string\n");
-    }
-
-    //Debugger code to see if flags set properly
-    //printf("%d%d%d%d\n", isInt, isNegative, isDec, isString);
-
-}
-
-// Global variable to track the number of shouts per thread
-int numShoutsGlobal;
-
-/*
-Shout
-When called by a thread it randomly shouts based on the number of threads ands how many shouts.
-Then it yields that thread in a buffer 2-5 times to allow other threads to shout.
-*/
-void Shout(int a)
-{
-  for(int x = 0; x < numShoutsGlobal; x++){
-
-    //Randomly picks one of five shouts to use
-    int shout = (Random()%5);
-    switch(shout)
-    {
-      case 0:
-      printf("%d Shout\n", a);
-      break;
-      case 1:
-      printf("%d Let it all out\n", a);
-      break;
-      case 2:
-      printf("%d These are the things I can do without\n", a);
-      break;
-      case 3:
-      printf("%d I'm talking to you\n", a);
-      break;
-      case 4:
-      printf("%d Come on\n", a);
-      break;
-    }
-
-    // Randomly picks the amount of time the currentThread will buffer (between 2-5).
-    int num = (Random()%5)+1;
-    if(num < 2)
-    {
-      num = (Random()%5)+1;
-    }
-
-    for(int x = 0; x < num; x++)
-    {
-      currentThread->Yield();
-    }
-
-  }
-}
-
-/*
-ThreadShout
-Based on user input passed to it this function creates that many threads and set the global number of shouts.
-Then Forks each thread to the shout function and passes its name as an int.
-*/
-void ThreadShout(int numThread, int numShout)
-{
-    Thread* t;
-    char id[2];
-    numShoutsGlobal = numShout;
-
-    // Creates a new thread based on user input and Forks it to shout
-    for(int x = 0; x < numThread; x++)
-    {
-        id[0] = (char)x;
-        t = new Thread(id);
-        t->Fork(Shout,x);
-    }
-
-}
-
-/*
-ThreadShoutInput
-A function that is called by a thread and verifies user input to create a thread shouting match.
-This verifies user input is between numbers 1-10,000.
-This then call a function passing the users values and creates threads that shout that many times.
-*/
-void ThreadShoutInput(int a)
-{
-  // Ask for user inputs
-  printf("Enter the numeber of threads you wish to make (1 to 10,000 max): ");
-
-  // Stores user input and stores it in memory as a character array and an integer value
-  // Gets the input for the number of Threads
-  char userInput[1025];
-  fgets(userInput, 1025, stdin);
-  int count = 0;
-  int userNumThread = atoi(userInput);
-
-  // Checks if the user is input a valid input
-  while('\0' != userInput[count] && '\n' != userInput[count])
-  {
-    if(userInput[count] == '0' || userInput[count] == '1' || userInput[count] == '2' || userInput[count] == '3' || userInput[count] == '4' || userInput[count] == '5'
-    || userInput[count] == '6' || userInput[count] == '7' || userInput[count] == '8' || userInput[count] == '9')
-    {
-      count++;
-    }else
-    {
-      printf("Not a valid input.\n");
-      printf("Enter the numeber of threads you wish to make (1 to 10,000 max): ");
-      fgets(userInput, 1025, stdin);
-      userNumThread = atoi(userInput);
-      count = 0;
-    }
-
-    if(userNumThread > 10000 || userNumThread < 1)
-    {
-      printf("Not a valid input.\n");
-      printf("Enter the numeber of threads you wish to make (1 to 10,000 max): ");
-      fgets(userInput, 1025, stdin);
-      userNumThread = atoi(userInput);
-      count = 0;
-    }
-  }
-
-  // Gets the input for the number of shouts
-    printf("Enter the number of shouts per thread (1 to 10,000): ");
-    fgets(userInput, 1025, stdin);
-    int userNumShout = atoi(userInput);
-    count = 0;
-
-      while('\0' != userInput[count] && '\n' != userInput[count])
-      {
-        if(userInput[count] == '0' || userInput[count] == '1' || userInput[count] == '2' || userInput[count] == '3' || userInput[count] == '4' || userInput[count] == '5'
-        || userInput[count] == '6' || userInput[count] == '7' || userInput[count] == '8' || userInput[count] == '9')
-        {
-          count++;
-        }else
-        {
-          printf("Not a valid input.\n");
-          printf("Enter the number of shouts per thread (1 to 10,000): ");
-          fgets(userInput, 1025, stdin);
-          userNumShout = atoi(userInput);
-          count = 0;
-        }
-
-        if(userNumShout > 10000 || userNumShout < 1)
-        {
-          printf("Not a valid input.\n");
-          printf("Enter the number of shouts per thread (1 to 10,000): ");
-          fgets(userInput, 1025, stdin);
-          userNumShout = atoi(userInput);
-          count = 0;
-        }
-      }
-      
-      ThreadShout(userNumThread,userNumShout);
-}
-
-void ThreadPick(int a)
-{
-
-}
 //----------------------Code by Chau Cao---------------------------
 //possValidation
 //helper function to evaluate user inputType
@@ -431,9 +154,27 @@ void possSendMail(int identifier) {
 
   printf("--------Person %d is trying to send: %s : to Person %d \n", identifier, randomMessage->message, randomRecipient);
   printf("---------Person %d is trying to claim freeSpaceSemaphore[%d]\n", identifier, randomRecipient);
+  poss.deadlock->P();
+  poss.deadlockMonitor[identifier] = true;
+  poss.deadlockCount++;
+  poss.deadlock->V();
+  if((poss.deadlockMonitor[randomRecipient] == true) && (poss.deadlockCount >= (poss.totalPeople / 2)))
+  {
+    printf("--------------!!Deadlock Prevention!!--------------\n");
+    if(poss.mailCount[identifier] == poss.sizeOfMailbox) {
+      possRead(identifier);
+    }
+    currentThread->Yield();
+  }
   freeSpaceSemaphore[randomRecipient]->P();
+  poss.deadlock->P();
+  poss.deadlockMonitor[identifier] = false;
+  poss.deadlockCount--;
+  poss.deadlock->V();
   printf("----------Person %d gets and decreases freeSpaceSemaphore[%d] by 1\n", identifier, randomRecipient);
+
   mailboxSemaphore[randomRecipient]->P();
+
   printf("-----------Person %d claims mailboxSemaphore[%d]\n", identifier, randomRecipient);
 
   //only one person is allowed to write at a time to not exceed the total messages alloted for simulation
@@ -510,7 +251,7 @@ void postOfficeSimSem(int identifier) {
   //loop each input until valid input is received
   //once validity determined assign values to global
   while(inputControl == 0){
-    printf("Enter the number of people: ");
+    printf("Enter the number of people (between 1 and 10000): ");
     fgets(P, 7, stdin);
     inputControl = possInputEval(P);
   }
@@ -518,7 +259,7 @@ void postOfficeSimSem(int identifier) {
 
   inputControl = 0;
   while(inputControl == 0) {
-    printf("Enter the capacity of the mailbox: ");
+    printf("Enter the capacity of the mailbox (between 1 and 10000): ");
     fgets(S, 7, stdin);
     inputControl = possInputEval(S);
   }
@@ -526,7 +267,7 @@ void postOfficeSimSem(int identifier) {
 
   inputControl = 0;
   while(inputControl == 0) {
-    printf("Enter total number of messages: ");
+    printf("Enter total number of messages (between 1 and 10000): ");
     fgets(M, 7, stdin);
     inputControl = possInputEval(M);
   }
@@ -541,25 +282,26 @@ void postOfficeSimSem(int identifier) {
   }
   //dynamically allocate and initialize relevant globals
   else {
-    poss.mailCount = new int[poss.totalPeople];
-    for(int x = 0; x < poss.totalPeople; x++) {
-      poss.mailCount[x] = 0;
-    }
+    //mailbox[][]
     poss.mailbox = new mail*[poss.sizeOfMailbox];
     for(int y = 0; y < poss.sizeOfMailbox; y++) {
       poss.mailbox[y] = new mail[poss.totalPeople];
     }
     poss.messagesSent = 0;
     poss.messagesRead = 0;
+    poss.deadlockCount = 0;
+    poss.deadlockMonitor = new bool[poss.totalPeople];
+    poss.deadlockCount = 0;
+    poss.deadlock = new Semaphore("deadlockSemaphore", 1);
     poss.writer = new Semaphore("writerSemaphore", 1);
-    //poss.reader = new Semaphore("readerSemaphore", poss.totalPeople);
+    poss.mailCount = new int[poss.totalPeople];
     freeSpaceSemaphore = new Semaphore *[poss.totalPeople];
-    for(int z = 0; z < poss.totalPeople; z++) {
-      freeSpaceSemaphore[z] = new Semaphore("freeSpaceSemaphore", poss.sizeOfMailbox);
-    }
     mailboxSemaphore = new Semaphore *[poss.totalPeople];
-    for(int n = 0; n < poss.totalPeople; n++) {
-      mailboxSemaphore[n] = new Semaphore("mailboxSemaphore", 1);
+    for (int x = 0; x < poss.totalPeople; x++) {
+      poss.deadlockMonitor[x] = false;
+      poss.mailCount[x] = 0;
+      freeSpaceSemaphore[x] = new Semaphore("freeSpaceSemaphore", poss.sizeOfMailbox);
+      mailboxSemaphore[x] = new Semaphore("mailboxSemaphore", 1);
     }
 
     for(int i = 0; i < poss.totalPeople; i++) {
@@ -572,33 +314,3 @@ void postOfficeSimSem(int identifier) {
   delete M;
 }
 //---------------------- End Code by Chau Cao---------------------------
-
-
-
-//----------------------------------------------------------------------
-// ThreadTest
-// 	Invoke a test routine.
-//----------------------------------------------------------------------
-
-void
-ThreadTest()
-{
-    DEBUG('t', "Entering ThreadTest");
-
-    Thread* t = new Thread("forked thread");
-    t->Fork(SimpleThread, 1);
-    SimpleThread(0);
-
-
-
-    if(myMenuOption == 1)
-    {
-      Thread* inputThread = new Thread("input thread");
-      inputThread->Fork(InputTest, 1);
-    }else if(myMenuOption == 2)
-    {
-      Thread* shoutInput = new Thread("shout input");
-      shoutInput->Fork(ThreadShoutInput, 1);
-    }
-
-}
