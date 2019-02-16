a5fbed031444a0b0161f053ef30e53cdb204d793
gfrilot123 <30584458+gfrilot123@users.noreply.github.com>
2019-02-16 16:46:36 -0600

    Update thread.h

--- thread.h
+++ thread.h
@@ -53,7 +53,7 @@
 
 // Size of the thread's private execution stack.
 // WATCH OUT IF THIS ISN'T BIG ENOUGH!!!!!
-#define StackSize	(4 * 10024)	// in words
+#define StackSize	(4 * 7024)	// in words
 
 
 // Thread state
